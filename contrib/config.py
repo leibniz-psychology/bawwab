31f680214737b3368270f1c941563835050698cb
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-04-20 09:28:02 +0200

    contrib: Improve sample config

--- contrib/config.py
+++ contrib/config.py
@@ -1,10 +1,17 @@
 # example configuration
 #SERVER_NAME = 'https://www.example.com'
-FORWARDED_SECRET = '123'
 LISTEN = 'localhost:8000'
 #SOCKET = 'bawwab.socket'
 SOCKET_MODE = 0o600
-DEBUG = True
+
+# set these to Falso for production
+DEBUG = False
+ACCESS_LOG = False
+
+# if deploying behind nginx add a Forwarded header (RFC 7239)
+# proxy_set_header Forwarded "for=_hidden;proto=https;by=_fooshyair5;host=$server_name";
+# see also https://sanic.readthedocs.io/en/latest/sanic/config.html#proxy-configuration
+FORWARDED_SECRET = '_foobar'
 FONTAWESOME_PATH = '/path/to/fontawesome'
 KNOWN_HOSTS_PATH = '/etc/ssh/ssh_known_hosts'
 
@@ -16,8 +23,11 @@ CLIENT_SECRET = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
 SCOPE = 'example'
 
 DATABASE_URL = 'sqlite://db.sqlite3'
-# password encryption key, use Fernet.generate_key() to generate one. If you
-# change or lose it, you cannot decrypt ssh passwords any more.
+# password encryption key, use
+# > from cryptography.fernet import Fernet
+# > Fernet.generate_key()
+# to generate one, see https://cryptography.io/en/latest/fernet/. If you change
+# or lose it, you cannot decrypt ssh passwords any more.
 DATABASE_PASSWORD_KEY = b'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 AUDIT_LOG_PATH = 'audit.log'
