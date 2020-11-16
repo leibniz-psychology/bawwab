leibniz-psychology/bawwab

commit 931f3e9d8daf1b650e99da1e267f9edb25afc4a0

Version 2
Complete rewrite of the app.

bawwab/action.py
@@ -4,9 +4,9 @@ Flexible one (or multiple) time action links
 Unicorns. Unicorns everywhere.
 """
 
-import asyncio
+import asyncio, hashlib
+from datetime import timedelta
 
-from yarl import URL
 from sanic import Blueprint
 from sanic.response import json
 from sanic.log import logger
@@ -15,63 +15,100 @@ from tortoise.models import Model
 from tortoise import fields
 
 from . import audit
-from .auth import GlobalPermission
-from .util import now
-from .workspace import defaultPermissions, Workspace, WorkspacePermissions
-from .tos import requireTos
+from .util import now, randomSecret
 
 class Action (Model):
-	token = fields.CharField (32, unique=True, description='Token used to trigger this action')
+	# token is hashed, so a database breach does not expose all actions and
+	# hexencoded, so SQL-lookups work
+	tokenHashed = fields.CharField (64*2, unique=True, description='Token used to trigger this action')
 	created = fields.DatetimeField (auto_now_add=True, description='Creation time')
 	expires = fields.DatetimeField (null=True, description='Expiry time')
 	usesRemaining = fields.IntField (null=True, description='Number of times the link can be used')
 	name = fields.CharField (16, description='Action name')
 	arguments = fields.JSONField (description='Action function parameters')
 
+	@staticmethod
+	def hashToken (t):
+		return hashlib.blake2b (t.encode ('ascii')).hexdigest ()
+
+	@property
+	def token (self):
+		raise NotImplementedError ()
+
+	@token.setter
+	def token (self, value):
+		self.tokenHashed = self.hashToken (value)
+
 bp = Blueprint('action')
 
-@bp.route ('/<token:string>', methods=['POST'])
-@requireTos
-async def executeAction (request, token):
-	config = request.app.config
-	session = request.ctx.session
+def actionToDict (token, action):
+	return dict (
+			token=token,
+			name=action.name,
+			expires=action.expires.isoformat (),
+			usesRemaining=action.usesRemaining,
+			)
 
-	action = await Action.get_or_none (token=token)
+@bp.route ('/', methods=['POST'])
+async def createAction (request):
+	session = request.ctx.session
+	form = request.json
+	name = form.get ('name')
+
+	if name == 'run':
+		# Why does a run action exist? When interacting with web services users
+		# expect they can share documents using links with other users who may
+		# or may not be registered on the platform already. For the underlying
+		# UNIX user model however, we cannot for instance grant permissions to
+		# a user that does not exist yet.
+		# Thus some form of “limited sudo” is required. Why not use
+		# passwordless sudo? Because it will not work with Kerberos and
+		# limiting the available commands is next to impossible.
+		# The command supports python-style format strings, see rpc.py
+		arguments = {'command': form['command'], 'user': session.authId}
+	else:
+		raise NotFound ({'status': 'invalid_action'})
+
+	token = randomSecret ()
+	action = Action (
+			expires=now()+timedelta (seconds=int (form['validFor'])),
+			usesRemaining=form['usesRemaining'],
+			name=form['name'],
+			arguments=arguments,
+			)
+	action.token = token
+	await action.save ()
+
+	return json (actionToDict (token, action))
+
+async def getAction (token):
+	action = await Action.get_or_none (tokenHashed=Action.hashToken (token))
 	if action is None:
-		raise NotFound ('token_not_found')
+		raise KeyError ()
 
 	# XXX: let a garbage cleaner remove expired ones?
 	if now () >= action.expires:
-		raise NotFound ('expired')
+		raise ValueError ('expired')
 	if action.usesRemaining is not None:
 		if action.usesRemaining <= 0:
-			raise NotFound ('expired')
+			raise ValueError ('expired')
 		action.usesRemaining -= 1
-		await action.save ()
+		await action.save (update_fields=('usesRemaining', ))
 
-	if action.name == 'modifyRolePermissions':
-		await GlobalPermission.filter (role=session.role).update (**action.arguments)
-	elif action.name == 'grantWorkspacePermissions':
-		# Grant permissions to the current role for the workspace in payload.
-		# Only *grants* permissions, but never removes them
-		workspace = await Workspace.get_or_none (id=action.arguments['workspace_id'])
-		if workspace is None:
-			raise NotFound ('workspace_not_found')
-		wp = await WorkspacePermissions.get_or_none (role=session.role, workspaces__id=action.arguments['workspace_id'])
-		if wp is None:
-			permissions = defaultPermissions.copy ()
-			permissions.update (action.arguments['permissions'])
-			wp = WorkspacePermissions (role=session.role, **permissions)
-			await wp.save ()
-			await workspace.permissions.add (wp)
-		else:
-			for k, v in action.arguments['permissions'].items ():
-				setattr (wp, k, v or getattr (wp, k))
-			await wp.save ()
-	else:
-		raise NotFound ('name_not_found')
-		
-	return json ({'name': action.name, 'arguments': action.arguments}, status=200)
+	return action
+
+@bp.route ('/<token:string>', methods=['GET'])
+async def fetchAction (request, token):
+	session = request.ctx.session
+
+	try:
+		action = await getAction (token)
+	except KeyError:
+		raise NotFound ('not_found')
+	except ValueError as e:
+		raise NotFound (e.args[0])
+
+	return json (actionToDict (token, action))
 
 __all__ = ['bp', 'runFactory']
 

bawwab/status.py
@@ -2,9 +2,7 @@ from sanic import Blueprint
 from sanic.response import json
 
 from .session import getStatus as getSession
-from .auth import getStatus as getAuth
-from .workspace import getStatus as getWorkspace
-from .application import getStatus as getApplication
+from .user import getStatus as getUser
 
 from .util import now
 
@@ -15,9 +13,7 @@ async def statusGet (request):
 	start = now ()
 	status = dict (
 			session=await getSession (),
-			user=await getAuth (),
-			workspace=await getWorkspace (),
-			application=await getApplication (),
+			user=await getUser (),
 			)
 	# we can abuse this to get a glimpse at database performance
 	status['status'] = dict (collecttime=(now()-start).total_seconds ())

bawwab/user.py
@@ -1,0 +1,204 @@
+"""
+User management.
+"""
+
+import asyncio, shlex
+from functools import wraps
+from datetime import timedelta
+
+from tortoise.models import Model
+from tortoise import fields
+from tortoise.exceptions import DoesNotExist, IntegrityError
+from tortoise.transactions import in_transaction
+from sanic import Blueprint
+from sanic.log import logger
+from sanic.response import html, json
+from sanic.exceptions import Forbidden, ServerError, NotFound, ServiceUnavailable
+import aiohttp
+from cryptography.fernet import Fernet
+import asyncssh
+
+from .util import randomSecret, now
+from . import audit
+
+bp = Blueprint ('user')
+
+class UserConnectionManager:
+	""" Manage per-user SSH connection """
+
+	def __init__ (self, host, knownHosts):
+		self._conns = {}
+		self.host = host
+		self.knownHosts = knownHosts
+
+	async def get (self, user):
+		conn = self._conns.get (user, None)
+		if not conn:
+			conn = await asyncssh.connect (
+					host=self.host,
+					port=22,
+					username=user.name,
+					password=user.password,
+					options=asyncssh.SSHClientConnectionOptions (known_hosts=self.knownHosts),
+					)
+			self._conns[user] = conn
+		return conn
+
+	async def aclose (self):
+		for c in self._conns.values ():
+			c.close ()
+			await c.wait_closed ()
+
+class User (Model):
+	crypter = None
+
+	authId = fields.CharField (128, null=True, unique=True, description='OAuth identity')
+	name = fields.CharField (64, description='UNIX user name')
+	passwordEncrypted = fields.BinaryField (description='UNIX user password')
+
+	@classmethod
+	def setup (cls, key):
+		# Obviously this is security through obscurity. But we want to make sure
+		# that someone with access to the database cannot gain SSH access unless he
+		# also has access to this application, which would be game over anyway. It
+		# would be nice if we could encrypt it with the user’s password, but – you
+		# know – OAuth.
+		cls.crypter = Fernet (key)
+
+	@property
+	def password (self):
+		return self.crypter.decrypt (self.passwordEncrypted).decode ('utf-8')
+
+	@password.setter
+	def password (self, password):
+		self.passwordEncrypted = self.crypter.encrypt (password.encode ('utf-8'))
+
+	async def getConnection (self):
+		return await connmgr.get (self)
+
+async def getStatus ():
+	""" Get module status information """
+	activeSince = now() - timedelta (days=1)
+	return dict (
+			total=await User.filter().count (),
+			anonymous=await User.filter(authId=None).count (),
+			login1d=await User.filter(lastLogin__gte=activeSince).count (),
+			)
+
+@bp.route ('/', methods=['GET'])
+async def userGet (request):
+	session = request.ctx.session
+
+	try:
+		user = await User.filter (authId=session.authId).first ()
+	except KeyError:
+		raise NotFound ('anonymous')
+
+	if not user:
+		raise NotFound ('nonexistent')
+
+	return json (dict (
+		name=user.name,
+		password=user.password,
+		))
+
+@bp.route ('/', methods=['POST'])
+async def userCreate (request):
+	session = request.ctx.session
+	form = request.json
+
+	try:
+		form['authorization'] = session.authId
+	except KeyError:
+		raise Forbidden ('anonymous')
+
+	user = await User.filter (authId=session.authId).first ()
+	if user:
+		raise Forbidden ('exists')
+
+	try:
+		async with request.app.usermgrd.post ('http://localhost/', json=form) as resp:
+			data = await resp.json ()
+			if data['status'] != 'ok':
+				audit.log ('user.create.usermgrd_error', reason=data['status'])
+				raise ServerError ('backend')
+	except aiohttp.ClientConnectionError:
+		audit.log ('user.create.usermgrd_connect_failure')
+		raise ServiceUnavailable ('backend')
+
+	user = User (authId=session.authId, name=data['user'])
+	user.password = data['password']
+	await user.save ()
+
+	return json (dict (
+		name=user.name,
+		password=user.password,
+		))
+
+@bp.route ('/', methods=['DELETE'])
+async def userDelete (request):
+	async def callDelete (expectedStatus):
+		try:
+			logger.debug (f'calling delete user for {user.name}')
+			async with request.app.usermgrd.delete (f'http://localhost/{user.name}') as resp:
+				data = await resp.json ()
+				logger.debug (f'got response {data}')
+				status = data['status']
+				if status == 'user_not_found':
+					logger.warning (f'user {user.name} was already gone')
+					return None
+				elif status != expectedStatus:
+					audit.log ('user.delete.usermgrd_error', reason=data['status'],
+							authId=authId, user=user.name)
+					raise ServerError ('backend')
+				return data
+		except aiohttp.ClientConnectionError:
+			audit.log ('user.delete.usermgrd_connect_failure', user=user.name,
+					authId=authId)
+			raise ServiceUnavailable ('backend')
+
+	session = request.ctx.session
+
+	authId = session.authId
+	user = await User.filter (authId=authId).first ()
+	if not user:
+		raise NotFound ('nonexistent')
+
+	data = await callDelete ('again')
+	if data:
+		# create the file
+		token = data['token']
+		try:
+			conn = await user.getConnection ()
+		except asyncssh.misc.PermissionDenied:
+			raise Forbidden ('locked_out')
+		command = ['touch', token]
+		logger.debug (f'running command {command}')
+		p = await conn.create_process (shlex.join (command))
+		await p.wait ()
+		p.close ()
+		await p.wait_closed ()
+		
+		# call again to confirm
+		await callDelete ('ok')
+
+	audit.log ('user.delete', user=user.name, authId=authId)
+	await user.delete ()
+
+	return json ({}, status=200)
+
+connmgr = None
+
+@bp.listener('before_server_start')
+async def setup (app, loop):
+	global connmgr
+
+	config = app.config
+	User.setup (config.DATABASE_PASSWORD_KEY)
+	connmgr = UserConnectionManager (host=config.SSH_HOST,
+			knownHosts=config.KNOWN_HOSTS_PATH)
+
+@bp.listener('after_server_stop')
+async def teardown (app, loop):
+	if connmgr:
+		await connmgr.aclose ()

bawwab/application.py
@@ -1,240 +1,0 @@
-import asyncio
-from traceback import TracebackException
-from datetime import timedelta
-
-import aiohttp
-import asyncssh
-from yarl import URL
-from sanic import Blueprint
-from sanic.response import json
-from sanic.log import logger
-from tortoise.models import Model
-from tortoise.functions import Count
-from tortoise import fields
-
-from . import audit
-from .util import now, randomSecret
-from .tos import requireTos
-
-class Application (Model):
-	"""
-	Runnable entity bound to a workspace
-	"""
-
-	key = fields.CharField (32, description='Application kind')
-	command = fields.CharField (128, description='Shell command that starts the application')
-	conductorKey = fields.CharField (32, description='Key (i.e. subdomain) for conductor')
-	lastStarted = fields.DatetimeField (null=True, description='Time the application was started')
-
-	def toPublicDict (self):
-		return dict (id=self.id, key=self.key)
-
-class ApplicationRunner:
-	"""
-	Run a single application on a remote server
-	"""
-
-	# XXX: this is ugly
-	CONFIG = None
-
-	def __init__ (self, application):
-		self.application = application
-		self.token = randomSecret (32)
-		self._task = None
-
-	async def baseUrl (self):
-		w = await self.application.workspaces.all ().first ()
-		return URL (self.CONFIG.PROXY_HOST.format (key=self.application.conductorKey, user=w.sshUser))
-
-	async def loginUrl (self):
-		if self.running:
-			return (await self.baseUrl ()).with_path (f'/_conductor/auth/{self.token}')
-		else:
-			return None
-
-	@property
-	def running (self):
-		return self._task and not self._task.done ()
-
-	def run (self):
-		self._task = asyncio.ensure_future (self._run ())
-
-	async def _run (self):
-		host = self.CONFIG.SSH_HOST
-		workspace = await self.application.workspaces.all ().first ()
-		user = workspace.sshUser
-
-		async def dumpLines (fd):
-			try:
-				while True:
-					l = await fd.readline ()
-					if not l:
-						break
-					logger.info (l.strip ())
-			except:
-				pass
-
-		try:
-			async with asyncssh.connect (
-					host,
-					port=22,
-					username=user,
-					password=workspace.sshPassword,
-					options=asyncssh.SSHClientConnectionOptions (known_hosts=self.CONFIG.KNOWN_HOSTS_PATH),
-					) as conn, \
-					conn.create_process (f'$SHELL -l -c "CONDUCTOR_TOKEN={self.token} {self.application.command}"') as proc:
-				stdoutTask = asyncio.ensure_future (dumpLines (proc.stdout))
-				stderrTask = asyncio.ensure_future (dumpLines (proc.stderr))
-				try:
-					ret = await proc.wait ()
-					audit.log ('application.exit', dict (
-							app=self.application.id,
-							command=self.application.command,
-							workspace=workspace.id,
-							returncode=ret.exit_status,
-							signal=ret.exit_signal,
-							))
-				except asyncio.CancelledError:
-					proc.terminate ()
-					await proc.wait ()
-					audit.log ('application.cancel', dict (
-							app=self.application.id,
-							command=self.application.command,
-							workspace=workspace.id,
-							))
-				await stdoutTask
-				await stderrTask
-		except Exception as e:
-			audit.log ('application.crash', dict (
-					app=self.application.id,
-					command=self.application.command,
-					workspace=workspace.id,
-					traceback=list (TracebackException.from_exception (e).format ()),
-					))
-
-class ApplicationRunnerFactory:
-	"""
-	This guy here manages running our applications. Obviously it’s not a good
-	idea to run it in the same process as the web application since a restart
-	kills all applications. But for now we’r doing it anyway.
-	"""
-
-	def __init__ (self):
-		self.running = {}
-
-	def get (self, application):
-		runner = self.running.get (application.id, None)
-		if runner and runner.running:
-			return runner
-		raise KeyError ()
-
-	async def start (self, application):
-		runner = self.running.get (application.id, None)
-		if runner:
-			if runner._task.done ():
-				await runner._task
-				runner = None
-		if not runner:
-			runner = ApplicationRunner (application)
-			runner.run ()
-			self.running[application.id] = runner
-
-			application.lastStarted = now ()
-			await application.save ()
-		return runner
-
-	async def stop (self, application):
-		runner = self.running.pop (application.id, None)
-		if runner:
-			runner._task.cancel ()
-			# XXX: time-out
-			await runner._task
-
-def defaultApplications ():
-	return [
-		Application (key='jupyterlab', command='startjupyter', conductorKey='jupyterlab'),
-		Application (key='rstudio', command='startrstudio', conductorKey='rstudio'),
-		]
-
-async def getStatus ():
-	""" Get module status information """
-	activeSince = now() - timedelta (days=1)
-
-	async def collapse (result):
-		result = await result.annotate(count=Count("key")).group_by("key").values("key", "count")
-		result = dict (map (lambda x: (x['key'], x['count']), result))
-		result['all'] = sum (result.values ())
-		return result
-
-	total = await collapse (Application)
-	active1d = await collapse (Application.filter(lastStarted__gte=activeSince))
-
-	return dict (
-			total=total,
-			active1d=active1d,
-			activeNow=len (runFactory.running),
-			)
-
-bp = Blueprint('application')
-runFactory = ApplicationRunnerFactory ()
-
-@bp.listener('before_server_start')
-async def setup (app, loop):
-	config = app.config
-	ApplicationRunner.CONFIG = config
-
-@bp.route ('/<aid:int>', methods=['POST'])
-@requireTos
-async def applicationStart (request, aid):
-	config = request.app.config
-	session = request.ctx.session
-
-	try:
-		a = await Application.filter (id=aid,
-				workspaces__permissions__role=session.role,
-				workspaces__permissions__canExecute=True).first ()
-		# XXX: well, one to many…
-		workspace = await a.workspaces.all().first ()
-	except DoesNotExist:
-		return json ({}, status=404)
-
-	audit.log ('application.start', dict (
-			app=a.id,
-			command=a.command,
-			session=session.name,
-			sshUser=workspace.sshUser,
-			))
-	runner = await runFactory.start (a)
-
-	appBaseUrl = await runner.baseUrl ()
-	# make sure it is actually available
-	timeout = aiohttp.ClientTimeout (total=10)
-	async with aiohttp.ClientSession(timeout=timeout) as client:
-		i = 0
-		maxRetries = 30
-		while True:
-			try:
-				logger.error (f'app {aid} checking status of {appBaseUrl}')
-				async with client.get(appBaseUrl,
-						cookies={'authorization': runner.token},
-						allow_redirects=False) as resp:
-					logger.info (f'app {aid} got status {resp.status}')
-					# positive response
-					if 200 <= resp.status < 400:
-						break
-			except aiohttp.ClientError:
-				logger.error (f'{aid} cannot connect to target application, retrying')
-			except asyncio.TimeoutError:
-				logger.error (f'{aid} timed out waiting for proxy')
-			i += 1
-			# died prematurely?
-			if i >= maxRetries or not runner.running:
-				return json ({}, status=500)
-			await asyncio.sleep (1)
-
-	adict = a.toPublicDict ()
-	adict['url'] = str (await runner.loginUrl ())
-	return json (adict, status=200)
-
-__all__ = ['bp', 'runFactory']
-

commit 41054d0c0f35df63859127c8d8c3b8d183afeabb

status: Fix status endpoint, add more information
Was broken since 931f3e9d8daf1b650e99da1e267f9edb25afc4a0

bawwab/action.py
@@ -59,6 +59,11 @@ class Action (Model):
 	def token (self, value):
 		self.tokenHashed = self.hashToken (value)
 
+async def getStatus ():
+	return dict (
+		total=await Action.filter().count(),
+		)
+
 bp = Blueprint('action')
 
 def actionToDict (token, action):

bawwab/process.py
@@ -25,6 +25,7 @@ Interact with processes running on the compute backend
 import shlex, json, asyncio, traceback
 from collections import defaultdict
 from functools import partial
+from itertools import chain
 
 from sanic import Blueprint
 from sanic.log import logger
@@ -112,6 +113,19 @@ class WebsocketProcess:
 		asyncio.create_task (sendOutput ('stderr', p.stderr))
 
 		self.task = asyncio.create_task (f (p))
+
+async def getStatus ():
+	replayBufferMessages = 0
+	for procs in perUserProcesses.values ():
+		for p in procs.values ():
+			replayBufferMessages += len (p.messages)
+
+	return dict (
+		users=len (perUserSockets),
+		sockets=sum (map (len, perUserSockets.values ())),
+		processes=sum (map (len, perUserProcesses.values ())),
+		replayBufferMessages=replayBufferMessages,
+		)
 
 @bp.route ('/', methods=['POST'])
 async def processRun (request):

bawwab/status.py
@@ -18,11 +18,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import asyncio, resource
+
 from sanic import Blueprint
 from sanic.response import json
 
 from .session import getStatus as getSession
 from .user import getStatus as getUser
+from .process import getStatus as getProcess
+from .action import getStatus as getAction
 
 from .util import now
 
@@ -31,9 +35,20 @@ bp = Blueprint ('status')
 @bp.route ('/')
 async def statusGet (request):
 	start = now ()
+
+	usage = resource.getrusage (resource.RUSAGE_SELF)
+	runtime = dict (
+		tasks=len (asyncio.all_tasks ()),
+		cpuTimeUser=usage.ru_utime,
+		cpuTimeSystem=usage.ru_stime,
+		)
+
 	status = dict (
 			session=await getSession (),
 			user=await getUser (),
+			process=await getProcess (),
+			action=await getAction (),
+			runtime=runtime,
 			)
 	# we can abuse this to get a glimpse at database performance
 	status['status'] = dict (collecttime=(now()-start).total_seconds ())

bawwab/user.py
@@ -101,8 +101,6 @@ async def getStatus ():
 	activeSince = now() - timedelta (days=1)
 	return dict (
 			total=await User.filter().count (),
-			anonymous=await User.filter(authId=None).count (),
-			login1d=await User.filter(lastLogin__gte=activeSince).count (),
 			)
 
 @bp.route ('/', methods=['GET'])